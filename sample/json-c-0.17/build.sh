440ef8d6a348c4b06be095bb5638587f71eaaadb
shecannotsee <shecannotsee@163.com>
2024-07-02 10:32:26 +0800

    [feat] 1.Add json-c-0.17 sample

---
+++ sample/json-c-0.17/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+project="sheer_third_party"
+project_length=${#project}
+# Get pwd string
+cd ..
+cd ..
+path=$(pwd)
+# Get last project_length chars
+suffix="${path: -$project_length}"
+if [ "$suffix" = "$project" ]; then
+    echo "ok"
+else
+    echo "Path error.Please ensure that the last few strings after using pwd are "$project
+    exit 1  # stop
+fi
+# check dir libraries
+echo "Check libraries exist?"
+if [ -d "./libraries" ];
+then
+    echo "libraries."
+else
+    echo "Create libraries."
+fi
+
+# get source code
+git clone https://github.com/json-c/json-c.git ./libraries/json-c/
+cd libraries
+mv json-c json-c-src
+mkdir json-c
+cd json-c
+install_path=$(pwd)
+cd ..
+
+# build
+cd json-c-src
+git checkout json-c-0.17-20230812
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX="$install_path" ..
+make -j8
+make install
+
+
+
+
+
+
+
+
+
