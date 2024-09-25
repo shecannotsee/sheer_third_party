6c312d73aa75a00a9416a20e1add53f68f4af7d1
shecannotsee <shecannotsee@163.com>
2024-09-25 14:08:57 +0800

    [fix] 1.move scripts: zlib

--- build_scripts/yaml-cpp/0.8.0.sh
+++ build_scripts/yaml-cpp/0.8.0.sh
@@ -2,8 +2,6 @@
 project="sheer_third_party"
 project_length=${#project}
 # Get pwd string
-cd ..
-cd ..
 path=$(pwd)
 # Get last project_length chars
 suffix="${path: -$project_length}"
@@ -26,8 +24,8 @@ fi
 git clone https://github.com/jbeder/yaml-cpp.git ./libraries/yaml-cpp/
 cd libraries
 mv yaml-cpp yaml-cpp-src
-mkdir yaml-cpp
-cd yaml-cpp
+mkdir yaml-cpp-0.8.0
+cd yaml-cpp-0.8.0
 install_path=$(pwd)
 cd ..
 
@@ -39,12 +37,3 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX="$install_path" ..
 make -j8
 make install
-
-
-
-
-
-
-
-
-
