161f240b51d973ba650d896cf9ad927d29efbe50
shecannotsee <shecannotsee@163.com>
2024-09-25 12:15:31 +0800

    [fix] 1.move scripts: jwt-cpp

---
+++ build_scripts/jwt-cpp/v0.7.0.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+project="sheer_third_party"
+project_length=${#project}
+# Get pwd string
+path=$(pwd)
+# Get last project_length chars
+suffix="${path: -$project_length}"
+if [ "$suffix" = "$project" ]; then
+    echo "ok"
+else
+    echo "Path error.Please ensure that the last few strings after using pwd are "$project
+    exit 1  # stop
+fi
+# check dir libraries
+echo "Check libraries exist?"
+if [ -d "./libraries" ];
+then
+    echo "libraries."
+else
+    echo "Create libraries."
+fi
+
+# get source code
+git clone https://github.com/Thalhammer/jwt-cpp.git ./libraries/jwt-cpp/
+cd libraries
+mv jwt-cpp jwt-cpp-src
+mkdir jwt-cpp-v0.7.0
+cd jwt-cpp-v0.7.0
+install_path=$(pwd)
+cd ..
+
+# build
+cd jwt-cpp-src
+git checkout v0.7.0
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX="$install_path" ..
+make -j8
+make install
