0f0aecbbc780a92926f089572487e11f0d3276d7
shecannotsee <shecannotsee@163.com>
2024-10-08 19:25:51 +0800

    [feat] 1.Add config function

--- build_utils/use_python/build_helper.py
+++ build_utils/use_python/build_helper.py
@@ -2,6 +2,14 @@ import os
 import subprocess
 from log import record, LOG_LEVEL
 
+def config(*args) -> str:
+    for arg in args:
+        if not isinstance(arg, str):
+            raise ValueError("All arguments must be strings")
+    command = ["./config"] + list(args)
+    subprocess.run(command, check=True)
+    return " ".join(command) # Format output command
+
 def configure(*args) -> str:
     for arg in args:
         if not isinstance(arg, str):
