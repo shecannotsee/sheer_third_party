aaaf01b98f51d0d34dbccf6f3853b8bb603f4d88
shecannotsee <shecannotsee@163.com>
2024-10-17 17:39:38 +0800

    [feat] 1.Add pgsql14.12 python build script 2.Add 3rd

--- build_scripts/PostgreSQL/14.12.sh
+++ build_scripts/PostgreSQL/14.12.sh
@@ -21,7 +21,8 @@ else
 fi
 
 # get source code
-git clone https://github.com/postgres/postgres.git ./libraries/PostgreSQL/
+git clone https://github.com/postgres/postgres.git ./libraries/PostgreSQL/ || \
+  { echo "Failed to clone postgres repository."; exit 1; }
 cd libraries
 mv PostgreSQL PostgreSQL-src
 mkdir PostgreSQL-14.12
@@ -31,7 +32,11 @@ cd ..
 
 # build
 cd PostgreSQL-src
-git checkout REL_14_12
-./configure --prefix="$install_path"
-make -j8
-make install
+git checkout REL_14_12 || \
+  { echo "Failed to checkout REL_14_12 of ffmpeg."; exit 1; }
+./configure --prefix="$install_path" || \
+  { echo "configure failed."; exit 1; }
+make -j8 || \
+  { echo "Build failed."; exit 1; }
+make install || \
+  { echo "Install failed."; exit 1; }
